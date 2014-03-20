b353436b228a8b6c3a3fb7b0614fc273256527d9
Natalie Cooper <ncooper@tcd.ie>
2014-03-20 21:11:48 +0000

    Added sampling effort dataset

--- Analyses/LifeHistoryPSR_analyses.R
+++ Analyses/LifeHistoryPSR_analyses.R
@@ -1,7 +1,7 @@
-library(ape)
+require(caper)
 source("PSR_functions.R")
 source("Useful_functions.R")
-source("SamplingEffort_function.R")
+source("SamplingEffort_functions.R")
 set.seed(1)
 
 #-------------------------------------------------------------------
@@ -16,8 +16,7 @@ set.seed(1)
 ds <- read.delim("squirrel.data.txt", header = TRUE)
 mammal.tree <- read.nexus("mammalST_MSW05_best_chrono.tre")
 PT <- read.delim("PanTHERIA05.txt")
-#LH data
-#citation count data
+sampling.effort <- read.delim("squirrel.sampling.txt", header = TRUE)
 
 #Identify variables
 host <- column.ID(ds, "HostCorrectedName")
@@ -36,7 +35,7 @@ ds <- remove.incomplete.data(ds, c(host,parasite,type))
 #Match tree and data
 ds <- replace.spaces(ds, host)
 ds <- remove.missing.species.tree(mammal.tree, ds, host)
-squirrel.tree <- remove.missing.species.data (mammal.tree, ds, host)
+squirrel.tree <- remove.missing.species.data(mammal.tree, ds, host)
 
 #Estimate PSR for all species, parasite types, and transmission modes
 psr.data <- unique.pairs(ds, parasite, host)
@@ -44,23 +43,25 @@ PSR <- psr(psr.data, parasite, host)
 PSR.type <- psr(psr.data, parasite, host, type)
 #PSR.transmode <- psr(ds, parasite, host, transmode)
 
+PSR.complete <- merge(PSR, PSR.type, by="host", all.x=TRUE, all.y=TRUE)
+
 #Estimate sampling effort for each host species
-#TO DO#
+hostname <- column.ID(sampling.effort, "HostCorrectedName")
+samples <- sampling.occ(sampling.effort, hostname)
 
 #Merge the datasets together
-PSR.complete <- merge(PSR, PSR.type, by.x = "host", by.y = "host")
-
-
-squirrel.data <- merge(PSR, PSR.type, by.x = "host", by.y = "host") 
+squirrel.data <- merge(PSR.complete, samples, by="host", all.x=TRUE, all.y=TRUE) 
+squirrel.data <- merge(squirrel.data, LHdata, by="host", all.x=TRUE, all.y=TRUE) 
 
 #-------------------------------------------------------------------
 #PGLS analyses
 #-------------------------------------------------------------------
 
-squirrel <- comparative.data()
+squirrel <- comparative.data(phy=squirrel.tree, data=squirrel.data, 
+	                         names.col=squirrel.data[[host]])
 
-model1 <- pgls(PSR ~ ln(BodySize) + ln(citations), data = squirrel, lambda = 'ML')
+model1 <- pgls(PSR ~ ln(BodySize) + ln(sampling.occ+0.1), data=squirrel, lambda='ML')
 summary(model1)
 plot(model1)
 
-plot(PSR ~ ln(BodySize), data = squirrel)
+plot(PSR ~ ln(BodySize), data=squirrel)
