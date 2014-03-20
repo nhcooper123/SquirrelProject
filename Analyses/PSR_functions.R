35e3ad97104d6008294f74fb65a47a20af973f46
Natalie Cooper <ncooper@tcd.ie>
2014-03-20 14:40:15 +0000

    Fixed output function so it gives sensible df output

--- Analyses/PSR_functions.R
+++ Analyses/PSR_functions.R
@@ -1,5 +1,7 @@
 #Useful generic functions 
 
+require(plyr)
+
 #Identify column numbers
 column.ID <- function(data, column.name) { 
   which(names(data)==column.name)
@@ -22,61 +24,36 @@ remove.blanks <- function(data, species.col) {
   data[which(data[species.col] != ""),]
 }
 
-#Functions to calculate PSR for data & subsets
+#Making data subsets
+get.subset <- function(data, subset.col, subset.no) { 
+    data[which(data[[subset.col]]==unique(data[[subset.col]])[subset.no]),]
+  }
+
+#Functions to calculate PSR
 
 #Calculating PSR for one host, with counting sp. where genus only occurs once
-count.parasites <- function(parasite) {
-  genus <- sub(" .*$", "", parasite)
-  species <- sub("^.* ", "", parasite)
-  sum(tapply(species, genus, function(spp)
-         if (length(spp) == 1) 1 else length(setdiff(spp, "sp."))))
+count.parasites <- function(x) {
+  genus <- sub(" .*$", "", x)
+  species <- sub("^.* ", "", x)
+  ans <- tapply(species, genus, function(spp)
+         if (length(spp) == 1) 1 else length(setdiff(spp, "sp.")))
+  sum(ans)
 }
 
-#PSR for all host species
+#PSR for all host species in a dataset
 PSR.all <- function(data, parasite.col, host.col) {
-  tapply(data[[parasite.col]], data[[host.col]], count.parasites)
+  aggregate(data[[parasite.col]] ~ data[[host.col]], data, count.parasites)
 }
-
-#PSR for just a subset of species
-PSR.subset <- function(data, parasite.col, host.col, subset.col) { 
-  for(i in seq_along(unique(data[[subset.col]]))) {
-    data.subset <- data[which(data[[subset.col]]==unique(data[[subset.col]])[i]),]
-    print(PSR.all(data.subset, parasite.col, host.col))
-  }
-}
-
-SPELT.data[i,3] is slower than SPELT.data[[3]][i]
 
 #Ouput for PSR calculations
 PSR.output <- function(output, subset.name=NULL) {
-  output <- data.frame(output)
-  output$host <- rownames(output)
-  names(output)[1] <- paste("PSR", subset.name, sep = "")
+  names(output) <- c("host", paste("PSR", subset.name, sep = ""))
   return(output)
 }
 
-#Combined function to run any PSR calculation
-PSR <- function(data, parasite.col, host.col, subset.col=NULL) {
-  if(subset.col!= NULL) {
-    psr <- PSR.subset(data, parasite.col, host.col, subset.col)
-  } else {	
-	psr <- PSR.all(data, parasite.col, host.col)
-  }	
+#Overall PSR function
+PSR <- function(data, parasite.col, host.col, subset.name=NULL) {
+  psr <- PSR.all(data, parasite.col, host.col)
+  PSR.output(psr, subset.name)
+  } 
 }
-
-
-
-
-
-
-
-psr <- tapply(data[,parasite], data[,host], FUN=length)
-#To write
-#Code to make output useable
-#code that can deal with sp.s in Genus and Species names
-#code that makes the dataset unique for pairs of species and genera
-#Reminder to people to check spelling and taxonomy
-
-
-
-
