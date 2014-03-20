869b80421c1727d3a1ef1ee6f5862ac116a759ab
Natalie Cooper <ncooper@tcd.ie>
2014-03-20 13:42:12 +0000

    Added function for counting parasites including genera with only sp.

--- Analyses/PSR_functions.R
+++ Analyses/PSR_functions.R
@@ -9,49 +9,43 @@ column.ID <- function(data, column.name) {
 
 #Remove replicated records of host-parasite pairs
 unique.pairs <- function(data, parasite.col, host.col) {
-  data[which(!duplicated(data[,c(host.col,parasite.col)])),]
+  data[which(!duplicated(data[c(host.col,parasite.col)])),]
 }
 
 #Remove all species with sp. not full Latin Binomial
 remove.sp <- function(data, species.col) {
-  data[-grep("sp.$", as.character(data[,species.col]),]
+  data[-grep("sp.$", as.character(data[[species.col]])),]
 }
 
 #Remove blank species names
 remove.blanks <- function(data, species.col) {
-  data <- data[-grep("sp.$", as.character(data[,species.col]),]###FIX####
+  data[which(data[species.col] != ""),]
 }
 
 #Functions to calculate PSR for data & subsets
 
-#PSR for all species
-PSR.all <- function(data, parasite.col, host.col) {
-  genus <- sub(" .*$", "", x)
-  species <- sub("^.* ", "", x)
-  tapply(data[,parasite.col], data[,host.col], function(spp)
-    if (length(spp) == 1) 1 else length(setdiff(spp, "sp.")))
+#Calculating PSR for one host, with counting sp. where genus only occurs once
+count.parasites <- function(parasite) {
+  genus <- sub(" .*$", "", parasite)
+  species <- sub("^.* ", "", parasite)
+  sum(tapply(species, genus, function(spp)
+         if (length(spp) == 1) 1 else length(setdiff(spp, "sp."))))
 }
 
-  count.parasites <- function(x) {
-  genus <- sub(" .*$", "", x)
-  species <- sub("^.* ", "", x)
-  tapply(species, genus, function(spp)
-         if (length(spp) == 1) 1 else length(setdiff(spp, "sp.")))
-} 
+#PSR for all host species
+PSR.all <- function(data, parasite.col, host.col) {
+  tapply(data[[parasite.col]], data[[host.col]], count.parasites)
+}
 
 #PSR for just a subset of species
-PSR.subset <- function(data, parasite.col, host.col, subset.col) {
-  host <- column.ID(data,host.col)
-  parasite <- column.ID(data, parasite.col)
-  subset <- column.ID(data, subset.col)
-  
-  for(i in seq_along(unique(subset.col))) {
-    psr <- tapply(data[,parasite][data[,subset]==unique(subset.col)[i]], data[,host][data[,subset]==unique(subset.col)[i]], FUN=length)
-    PSR.output(psr, subset.name = unique(subset.col)[i])
+PSR.subset <- function(data, parasite.col, host.col, subset.col) { 
+  for(i in seq_along(unique(data[[subset.col]]))) {
+    data.subset <- data[which(data[[subset.col]]==unique(data[[subset.col]])[i]),]
+    print(PSR.all(data.subset, parasite.col, host.col))
   }
 }
 
-
+SPELT.data[i,3] is slower than SPELT.data[[3]][i]
 
 #Ouput for PSR calculations
 PSR.output <- function(output, subset.name=NULL) {
@@ -60,7 +54,6 @@ PSR.output <- function(output, subset.name=NULL) {
   names(output)[1] <- paste("PSR", subset.name, sep = "")
   return(output)
 }
-
 
 #Combined function to run any PSR calculation
 PSR <- function(data, parasite.col, host.col, subset.col=NULL) {
@@ -77,7 +70,7 @@ PSR <- function(data, parasite.col, host.col, subset.col=NULL) {
 
 
 
-
+psr <- tapply(data[,parasite], data[,host], FUN=length)
 #To write
 #Code to make output useable
 #code that can deal with sp.s in Genus and Species names
