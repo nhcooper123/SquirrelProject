ec3b74d134ebac35d453bd17f4c60853696137ee
Natalie Cooper <ncooper@tcd.ie>
2014-03-25 09:50:18 +0000

    Tidied up functions according to Google R Style guide

--- Analyses/LifeHistoryPSR_analyses.R
+++ Analyses/LifeHistoryPSR_analyses.R
@@ -1,17 +1,16 @@
+# Running analyses of PSR ~ life history variables
+# Currently this only keeps lines of the data
+# which have full species names for squirrels, 
+# where the squirrel is in the tree,
+# and parasites with transmission modes and types
+
 require(caper)
 source("PSR_functions.R")
 source("Useful_functions.R")
 source("SamplingEffort_functions.R")
 set.seed(1)
 
-#-------------------------------------------------------------------
-#Prepping data for PGLS analyses
-
-#Currently this only keeps lines of the data
-#which have full species names for squirrels, 
-#where the squirrel is in the tree,
-#and parasites with transmission modes and types
-#-------------------------------------------------------------------
+# Read in datasets (will need to setwd or similar)
 
 ds <- read.delim("squirrel.data.txt", header = TRUE)
 american.species <- read.delim("SquirrelsofAmerica.txt", header = FALSE)
@@ -19,8 +18,8 @@ mammal.tree <- read.nexus("mammalST_MSW05_best_chrono.tre")
 LHdata <- read.delim("SquirrelLifeHistory.txt", header = TRUE)
 sampling.effort <- read.delim("SquirrelSamplingEffort.txt", header = TRUE)
 
-#FULL DATASET
-#Identify variables
+# 1: FULL DATASET cleaning
+# Identify variables
 host <- column.ID(ds, "HostCorrectedName")
 parasite <- column.ID(ds, "ParasiteCorrectedName")
 type <- column.ID(ds, "ParasiteType")
@@ -31,11 +30,11 @@ vertical <- column.ID(ds, "Vertical")
 sexual <- column.ID(ds, "Sexual")
 close <- column.ID(ds, "CloseT")
 
-#Subset out just the american species
+# Subset out just the american species
 american.sp <- match(ds[[host]], american.species[[1]], nomatch = 0)
-ds <- subset(ds, american.sp!=0)
+ds <- subset(ds, american.sp != 0)
 
-#Tidy up the data 
+# Tidy up the data 
 ds <- remove.sp(ds, host)
 ds <- remove.blanks(ds, host)
 ds <- remove.blanks(ds, parasite)
@@ -44,67 +43,69 @@ ds <- remove.incomplete.data(ds, c(host,parasite,type,
 	                               vector, intermediate, nonclose,
 	                               vertical, sexual, close))
 
-#Match tree and data
+# Match tree and data
 ds <- replace.spaces(ds, host)
 ds <- remove.missing.species.data(mammal.tree, ds, host)
 squirrel.tree <- remove.missing.species.tree(mammal.tree, ds, host)
 
-#PSR DATA
-#Estimate PSR for all species, parasite types, and transmission modes
-#Then merge to make a dataset with all PSR results
+# 2: Extract PSR DATA
+# Estimate PSR for all species, parasite types, and transmission modes
+# Then merge to make a dataset with all PSR results
 psr.data <- unique.pairs(ds, parasite, host)
 PSR <- psr(psr.data, parasite, host)
 PSR.type <- psr(psr.data, parasite, host, type)
-PSR.vector <- psr(ds, parasite, host, vector, binary=TRUE)
-PSR.vertical <- psr(ds, parasite, host, vertical, binary=TRUE)
-PSR.intermediate <- psr(ds, parasite, host, intermediate, binary=TRUE)
-PSR.close <- psr(ds, parasite, host, close, binary=TRUE)
-PSR.nonclose <- psr(ds, parasite, host, nonclose, binary=TRUE)
-PSR.sexual <- psr(ds, parasite, host, sexual, binary=TRUE)
+PSR.vector <- psr(ds, parasite, host, vector, binary = TRUE)
+PSR.vertical <- psr(ds, parasite, host, vertical, binary = TRUE)
+PSR.intermediate <- psr(ds, parasite, host, intermediate, binary = TRUE)
+PSR.close <- psr(ds, parasite, host, close, binary = TRUE)
+PSR.nonclose <- psr(ds, parasite, host, nonclose, binary = TRUE)
+PSR.sexual <- psr(ds, parasite, host, sexual, binary = TRUE)
 
 list.of.PSR.results <- list(PSR, PSR.type, PSR.vector, PSR.vertical,
 							PSR.intermediate, PSR.sexual, PSR.close,
 							PSR.nonclose)
 
-PSR.complete <- Reduce(function(...) merge(..., by="host", all=TRUE), 
+PSR.complete <- Reduce(function(...) merge(..., by = "host", all = TRUE), 
 	                   list.of.PSR.results)
-PSR.complete[is.na(PSR.complete)]<-0
+PSR.complete[is.na(PSR.complete)] <- 0
 
-#SAMPLING EFFORT DATA
-#Identify variables
+# 3: SAMPLING EFFORT DATA
+# Identify variables
 hostname <- column.ID(sampling.effort, "HostCorrectedName")
 
-#Estimate sampling effort for each host species
+# Estimate sampling effort for each host species
 samples <- sampling.occ(sampling.effort, hostname)
 samples <- replace.spaces(samples, hostname)
 
-#COMBINE DATASETS
-#Merge the datasets together
-squirrel.data <- merge(PSR.complete, samples, by="host", all.x=TRUE) 
+# 4: COMBINE DATASETS
+# Merge the datasets together
+squirrel.data <- merge(PSR.complete, samples, by = "host", all.x = TRUE) 
 
-squirrel.data <- merge(squirrel.data, LHdata, by.x="host", 
-	                   by.y="MSW05_Binomial" all.x=TRUE) 
+squirrel.data <- merge(squirrel.data, LHdata, by.x = "host", 
+	                   by.y = "MSW05_Binomial" all.x = TRUE) 
 
-#And finally...make comparative data object for PGLS
+# 5: And finally...make comparative data object for PGLS
 
-squirrel <- comparative.data(phy=squirrel.tree, data=squirrel.data, 
-	                         names.col=squirrel.data[[host]])
+squirrel <- comparative.data(phy = squirrel.tree, data = squirrel.data, 
+	                         names.col = squirrel.data[[host]])
 
 #-------------------------------------------------------------------
-#PGLS analyses
+# PGLS analyses
 #-------------------------------------------------------------------
-#Example: Total parasite species richness and body size
-#With sampling occassions to control for sampling effort
+# Example: Total parasite species richness and body size
+# With sampling occassions to control for sampling effort
 
-model1 <- pgls(log(PSR) ~ log(BodyMass_g) + log(sampling.occ), data=squirrel, lambda='ML')
+model1 <- pgls(log(PSR) ~ log(BodyMass_g) + log(sampling.occ), 
+	           data = squirrel, lambda = 'ML')
 summary(model1)
 plot(model1)
 
-plot(log(PSR) ~ log(BodyMass_g), data=squirrel)
+plot(log(PSR) ~ log(BodyMass_g), data = squirrel)
 
-#Example: Helminth parasite species richness and body size
-#With sampling occassions to control for sampling effort
+# Example: Helminth parasite species richness and body size
+# With sampling occassions to control for sampling effort
 
-model2 <- pgls(log(PSRhelminth) ~ log(BodyMass_g) + log(sampling.occ), data=squirrel, lambda='ML')
+model2 <- pgls(log(PSRhelminth) ~ log(BodyMass_g) + log(sampling.occ), 
+	           data = squirrel, lambda = 'ML')
 summary(model2)
 plot(model2)
--- Analyses/PSR_examples.R
+++ Analyses/PSR_examples.R
@@ -1,5 +1,5 @@
-#Examples for using PSR functions
-#Data is available in SquirrelProject/Data
+# Examples for using PSR functions
+# Data is available in SquirrelProject/Data
 
 source("PSR_functions.R")
 source("Useful_functions.R")
@@ -7,11 +7,11 @@ source("Useful_functions.R")
 set.seed(1)
 ds <- read.delim("squirrel.data.txt", header = TRUE)
 
-#1: Clean up your dataset
-#Identify your variables
-#Remove replicated records of host-parasite pairs
-#Remove host species with sp. not full Latin Binomial
-#Remove blank species names
+# 1: Clean up your dataset
+# Identify your variables
+# Remove replicated records of host-parasite pairs
+# Remove host species with sp. not full Latin Binomial
+# Remove blank species names
 
 host <- column.ID(ds, "HostCorrectedName")
 parasite <- column.ID(ds, "ParasiteCorrectedName")
@@ -21,14 +21,14 @@ ds <- remove.sp(ds, host)
 ds <- remove.blanks(ds, host)
 ds <- remove.blanks(ds, parasite)
 
-#2: PSR for all types of parasite
+# 2: PSR for all types of parasite
 
 PSR <- psr(ds, parasite, host)
 PSR
 
-#3: PSR for different parasite types
-#Identify column to subset data with
-#Remove blank values for subset column
+# 3: PSR for different parasite types
+# Identify column to subset data with
+# Remove blank values for subset column
 
 type <- column.ID(ds, "ParasiteType")
 ds <- remove.blanks(ds, type)
@@ -36,12 +36,12 @@ ds <- remove.blanks(ds, type)
 PSR.type <- psr(ds, parasite, host, type)
 PSR.type
 
-#4: PSR for different vectors
-#Identify column to subset data with
-#Note that this is different because the data is binary
-#rather than categories like ParasiteType
+# 4: PSR for different vectors
+# Identify column to subset data with
+# Note that this is different because the data is binary
+# rather than categories like ParasiteType
 
 vector <- column.ID(ds, "Vector")
 
-PSR.vector <- psr(ds, parasite, host, vector, binary=TRUE)
+PSR.vector <- psr(ds, parasite, host, vector, binary = TRUE)
 PSR.vector
